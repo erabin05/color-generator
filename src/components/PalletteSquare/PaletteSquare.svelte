5729e99e2cbcffa6a3ebde96408388de36aab07c
Etienne Rabin <etienne.rabin_ext@michelin.com>
2025-01-20 10:40:18 +0100

    add copy button

--- src/components/PalletteSquare/PaletteSquare.svelte
+++ src/components/PalletteSquare/PaletteSquare.svelte
@@ -3,6 +3,10 @@
     export let scale: number
     export let isBase: boolean
     export let isShade: boolean
+
+    const onCopyClick = () => {
+        navigator.clipboard.writeText(hex)
+    }
 </script>
 
 <div
@@ -12,8 +16,14 @@
     style:background-color={hex}
     style:color={scale < 50 ? "#000" : "#FFF"}
 >
-    <p class='scale-name'>{scale}</p>
-    <p class='scale-hex'>{hex}</p>
+    <button
+        on:click={onCopyClick}
+        class:dark={scale < 50}
+    >copy</button>
+    <div class='info-container'>
+        <p class='scale-name'>{scale}</p>
+        <p class='scale-hex'>{hex}</p>
+    </div>
 </div>
 
 <style lang="scss">
